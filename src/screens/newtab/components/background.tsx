8c672fb1a61f4846c51e781c99a48e9aff641a68
Fadi Asaad <firon1222@gmail.com>
2023-10-26 21:41:25 +0300

    change wallpaper image and update glass filter values

--- src/screens/newtab/components/background.tsx
+++ src/screens/newtab/components/background.tsx
@@ -2,10 +2,10 @@ function Background() {
 	return (
 		<div className="top-0 left-0 z-[-1] fixed w-screen h-screen bg-black">
 			<div
-				className="w-full h-full absolute
-			bg-[url(https://w.wallhaven.cc/full/ne/wallhaven-ne2vow.jpg)]"
+				className="w-full h-full absolute bg-cover
+			bg-[url(/background.jpg)]"
 			></div>
-			<div className="w-full h-full absolute backdrop-blur bg-black/40"></div>
+			<div className="w-full h-full absolute backdrop-blur-sm bg-black/20"></div>
 		</div>
 	);
 }
