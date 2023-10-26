13308ba3655167159d98ebc2dd4258a6e5005a57
Fadi Asaad <firon1222@gmail.com>
2023-10-26 21:13:44 +0300

    update newtab screen with clock and background components, also update layout

--- src/screens/newtab/newtab.tsx
+++ src/screens/newtab/newtab.tsx
@@ -1,80 +1,41 @@
 import "@/main.css";
-import { useState, useEffect } from "react";
+import Clock from "./components/clock";
+import Background from "./components/background";
 
-// function useBrowserTabs() {
-// 	const [tabs, setTabs] = useState<browser.tabs.Tab[]>([]);
-// 	const [isLoading, setIsLoading] = useState(true);
-//
-// 	useEffect(() => {
-// 		browser.tabs
-// 			.query({})
-// 			.then(setTabs)
-// 			.finally(() => setIsLoading(false));
-// 	}, []);
-//
-// 	return { tabs, isLoading };
-// }
-
-// function TabItem({ tab }: { tab: browser.tabs.Tab }) {
-// 	return (
-// 		<li>
-// 			<h6>{tab.title}</h6>
-// 			<p>{tab.url}</p>
-// 		</li>
-// 	);
-// }
-
-// function Tabs() {
-// 	const { tabs, isLoading } = useBrowserTabs();
-//
-// 	return (
-// 		<ul>
-// 			{isLoading ? (
-// 				<p>Loading...</p>
-// 			) : (
-// 				tabs.map(tab => <TabItem key={`{tab.id}-${tab.url}`} tab={tab} />)
-// 			)}
-// 		</ul>
-// 	);
-// }
-
-function Clock() {
-	const [date, setDate] = useState<Date>(new Date());
-	const m = date.getMinutes();
-	const h = date.getHours();
-	const dp: string = h > 12 ? "PM" : "AM";
-
-	// update clock every second
-	useEffect(() => {
-		const timerId = setInterval(() => setDate(new Date()), 1000);
-		return () => clearInterval(timerId);
-	}, []);
-
-	return <div className="flex items-center font-bold">
-		<span className="p-4 text-9xl">{h < 10 ? "0" + h : h}</span>
-		<div className="flex flex-col text-5xl text-center">
-			<span>{dp}</span>
-			<span>{m < 10 ? "0" + m : m}</span>
-		</div>
-	</div>;
-}
-
-function Background() {
-	return <div className="top-0 left-0 z-[-1] fixed w-screen h-screen bg-black">
-		<div className="w-full h-full absolute
-			bg-[url(https://w.wallhaven.cc/full/ne/wallhaven-ne2vow.jpg)]"></div>
-		<div className="w-full h-full absolute backdrop-blur bg-black/40"></div>
-	</div>;
+function SimpleButton({
+	label,
+	onClick
+}: {
+	label: string;
+	onClick: () => void;
+}) {
+	return (
+		<button
+			className="hover:bg-white/20 p-2 rounded cursor-pointer"
+			onClick={onClick}
+		>
+			{label}
+		</button>
+	);
 }
 
 function NewTab() {
 	return (
 		<div className="w-screen h-screen">
 			<Background />
-			<div className="text-white flex flex-col items-center justify-center">
+			<div className="h-full text-white flex flex-col justify-between
+				items-center">
+				<div></div>
 				<Clock />
+				<div className="w-full h-12 px-2 flex justify-between items-center">
+					<SimpleButton label="Settings" onClick={() => { }} />
+					<div className="space-x-4">
+						<SimpleButton label="Sessions" onClick={() => { }} />
+						<SimpleButton label="Todo" onClick={() => { }} />
+					</div>
+				</div>
 			</div>
-		</div >
+		</div>
 	);
 }
 
