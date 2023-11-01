68fcce477b0ee70f450183dc7e3cda3135437d91
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-11-01 20:58:25 +0200

    added Todo List

--- src/screens/newtab/components/todo_section.tsx
+++ src/screens/newtab/components/todo_section.tsx
@@ -1,20 +1,29 @@
 import { useTodoManager } from "../features/todo_manager";
 
 function TodoSection() {
-	const { items, removeTodo } = useTodoManager();
+  const { items, removeTodo } = useTodoManager();
 
-	return (
-		<div>
-			<h1>Todo</h1>
-			<ul>
-				{items.map(todo => (
-					<li key={todo.id} onClick={() => removeTodo(todo.id)}>
-						{`${todo.title} - ${todo.id}`}
-					</li>
-				))}
-			</ul>
-		</div>
-	);
+  return (
+    <div className="w-full overflow-auto">
+      <h1 className="text-2xl text-center h-[5%] bg-black/80 w-full">
+        Todo List
+      </h1>
+      <ul className="h-[87%] w-full overflow-auto">
+        {items.map((todo) => (
+          <li
+            className="w-full bg-black/50 p-[10px] border-t border-l border-r border-gray-100/50 border-4 flex flex-wrap items-center justify-between"
+            key={todo.id}
+          >
+            <input type="checkbox" className="w-[10%] inline mr-2 ml-1 pt-[5px] peer h-4 w-4"/>
+            <div className="w-[80%] inline peer-checked:line-through">{`${todo.title} - ${todo.id}`}</div>
+            <div className="w-[10%] inline">
+              <button onClick={() => removeTodo(todo.id)}><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0,0,256,256"><g fill="#ff0000" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none"><g transform="scale(8.53333,8.53333)"><path d="M13,3c-0.26757,-0.00363 -0.52543,0.10012 -0.71593,0.28805c-0.1905,0.18793 -0.29774,0.44436 -0.29774,0.71195h-5.98633c-0.36064,-0.0051 -0.69608,0.18438 -0.87789,0.49587c-0.18181,0.3115 -0.18181,0.69676 0,1.00825c0.18181,0.3115 0.51725,0.50097 0.87789,0.49587h18c0.36064,0.0051 0.69608,-0.18438 0.87789,-0.49587c0.18181,-0.3115 0.18181,-0.69676 0,-1.00825c-0.18181,-0.3115 -0.51725,-0.50097 -0.87789,-0.49587h-5.98633c0,-0.26759 -0.10724,-0.52403 -0.29774,-0.71195c-0.1905,-0.18793 -0.44836,-0.29168 -0.71593,-0.28805zM6,8v16c0,1.105 0.895,2 2,2h14c1.105,0 2,-0.895 2,-2v-16z"></path></g></g></svg></button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default TodoSection;
--- src/screens/newtab/newtab.tsx
+++ src/screens/newtab/newtab.tsx
@@ -7,42 +7,52 @@ import TodoSection from "./components/todo_section";
 import { useState } from "react";
 import { useTodoManagerContext } from "./features/todo_manager";
 
-
-
 function NewTab() {
-	const [isQuickLinksOpened, setIsQuickLinksOpened] = useState<boolean>(false);
-	const { manager } = useTodoManagerContext();
+  const [isQuickLinksOpened, setIsQuickLinksOpened] = useState<boolean>(false);
+  const { manager } = useTodoManagerContext();
+  const [todoListVisible, setTodoListVisible] = useState(false);
 
-	return (
-		<div className="w-screen h-screen">
-			<Background />
-			<div
-				className="h-full text-white flex flex-col justify-between
+  const handleTodoButtonClick = () => {
+    setTodoListVisible(!todoListVisible);
+  };
+
+  return (
+    <div className="w-screen h-screen">
+      <Background />
+      <div
+        className="h-full text-white flex flex-col justify-between
 				items-center"
-			>
-				<div></div>
-				<Clock />
-				{isQuickLinksOpened && <QuickLinks />}
-				<TodoSection />
-				<div className="w-full h-12 px-2 flex justify-between items-center">
-					<div className="space-x-4">
-						<SimpleButton label="Settings" onClick={() => { }} />
-						<SimpleButton
-							label="Quick Links"
-							onClick={() => setIsQuickLinksOpened(s => !s)}
-						/>
-					</div>
-					<div className="space-x-4">
-						<SimpleButton label="Sessions" onClick={() => { }} />
-						<SimpleButton
-							label="Todo"
-							onClick={() => manager.addItem("New Todo")}
-						/>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+      >
+        <div></div>
+        <Clock />
+        {isQuickLinksOpened && <QuickLinks />}
+        <div
+          className="hidden"
+          style={{ display: todoListVisible ? "block" : "none" }}
+        >
+          <div className="absolute flex justify-evenly bg-black/30 w-[22%] h-[88%] right-[0px] bottom-[40px]">
+            <TodoSection />
+            <button className="addButton absolute w-full text-2xl bottom-[0px] bg-black/80 pb-3 pt-3 hover:text-gray-100/50 h-[8%]" onClick={() => manager.addItem("New Todo")}>
+              <span className="border-4 border-current text-2xl mr-2 pb-1">+</span>Add
+            </button>
+          </div>
+        </div>
+        <div className="w-full h-12 px-2 flex justify-between items-center">
+          <div className="space-x-4">
+            <SimpleButton label="Settings" onClick={() => {}} />
+            <SimpleButton
+              label="Quick Links"
+              onClick={() => setIsQuickLinksOpened((s) => !s)}
+            />
+          </div>
+          <div className="space-x-4">
+            <SimpleButton label="Sessions" onClick={() => {}} />
+            <SimpleButton label="Todo" onClick={handleTodoButtonClick} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NewTab;
