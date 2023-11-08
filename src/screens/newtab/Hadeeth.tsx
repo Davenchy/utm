876b5b0ae8024f75cef26e50f3bd068e2cf4823e
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-11-08 14:39:07 +0200

    Implement the Hadeeth feature

---
+++ src/screens/newtab/Hadeeth.tsx
@@ -0,0 +1,7 @@
+function Hadeeth(props: { content: string }) {
+    return (
+        <p className="hadeeth-content">{props.content}</p>
+    )
+}
+
+export default Hadeeth;
--- src/screens/newtab/main.tsx
+++ src/screens/newtab/main.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import NewTab from "./newtab";
+import Hadeeth from "./Hadeeth";
+import request from "request";
 
 const rootEl = document.getElementById("root");
+
+const url = "https://cdn.jsdelivr.net/gh/fawazahmed0/hadith-api@1/editions/eng-abudawud/1035.min.json";
+
+request(url, (error, res) => {
+	if (error) {
+		console.error("url error");
+		return;
+	}
+	const hadeethContent = res.body;
+	console.log(hadeethContent);
+});
+
 ReactDOM.createRoot(rootEl!).render(
-	<React.StrictMode>
-		<NewTab />
-	</React.StrictMode>
-);
+		<React.StrictMode>
+			<Hadeeth content={'dafafn'} />
+		</React.StrictMode>
+	);
