5d7270573fe26bfc217370416ac771e0b460cedc
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-11-08 16:57:28 +0200

    fix

--- src/screens/newtab/Hadeeth.tsx
+++ src/screens/newtab/Hadeeth.tsx
@@ -1,6 +1,26 @@
-function Hadeeth(props: { content: string }) {
+import { useEffect, useState } from "react";
+
+function Hadeeth() {
+    const [hadeeth, setHadeeth] = useState([]);
+
+    useEffect(() => {
+        const hadeethNum: number = Math.floor(Math.random() * (1034) + 1);
+        const url: string = "https://cdn.jsdelivr.net/gh/fawazahmed0/hadith-api@1/editions/eng-abudawud/"
+                            + hadeethNum + ".min.json";
+
+        console.log(hadeethNum);
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(typeof data);
+                console.log(data);
+                setHadeeth(data['hadiths'][0]['text']);
+            })
+            .catch(err => console.log(err))
+    }, [])
+
     return (
-        <p className="hadeeth-content">{props.content}</p>
+        <p className="hadeeth-content">{hadeeth}</p>
     )
 }
 
--- src/screens/newtab/main.tsx
+++ src/screens/newtab/main.tsx
@@ -1,23 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Hadeeth from "./Hadeeth";
-import request from "request";
 
 const rootEl = document.getElementById("root");
 
-const url = "https://cdn.jsdelivr.net/gh/fawazahmed0/hadith-api@1/editions/eng-abudawud/1035.min.json";
-
-request(url, (error, res) => {
-	if (error) {
-		console.error("url error");
-		return;
-	}
-	const hadeethContent = res.body;
-	console.log(hadeethContent);
-});
-
 ReactDOM.createRoot(rootEl!).render(
 		<React.StrictMode>
-			<Hadeeth content={'dafafn'} />
+			<Hadeeth />
 		</React.StrictMode>
-	);
+);
