18562fb912a641734db8b3cfcf1605d8e57bd75b
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-11-13 12:38:15 +0200

    Add Arabic text and Hadeeth details

--- src/screens/newtab/Hadeeth.tsx
+++ src/screens/newtab/Hadeeth.tsx
@@ -1,26 +1,52 @@
 import { useEffect, useState } from "react";
 
 function Hadeeth() {
-    const [hadeeth, setHadeeth] = useState([]);
+    const [hadeethara, setHadeethara] = useState([]);
+    const [hadeetheng, setHadeetheng] = useState([]);
+    const [hadeethDetails, setHadeethDetails] = useState("");
 
     useEffect(() => {
-        const hadeethNum: number = Math.floor(Math.random() * (1034) + 1);
-        const url: string = "https://cdn.jsdelivr.net/gh/fawazahmed0/hadith-api@1/editions/eng-abudawud/"
-                            + hadeethNum + ".min.json";
+        const hadeethNum: number = Math.floor(Math.random() * (7563) + 1);
 
-        console.log(hadeethNum);
+        let url: string = "https://cdn.jsdelivr.net/gh/fawazahmed0/hadith-api@1/editions/ara-bukhari/"
+            + hadeethNum + ".min.json";
+
         fetch(url)
             .then(res => res.json())
             .then(data => {
-                console.log(typeof data);
-                console.log(data);
-                setHadeeth(data['hadiths'][0]['text']);
+                if (data)
+                {
+                    console.log(data)
+                    setHadeethara(data['hadiths'][0]['text']);
+                    setHadeethDetails("Sahih al Bukhari, " + "Book #" + Object.keys(data["metadata"]["section"])[0]
+                        + " " + Object.values(data["metadata"]["section"])[0] + ", Hadeeth #" + hadeethNum);
+                }
             })
             .catch(err => console.log(err))
+
+        url = "https://cdn.jsdelivr.net/gh/fawazahmed0/hadith-api@1/editions/eng-bukhari/"
+            + hadeethNum + ".min.json";
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                if (data)
+                {
+                    setHadeetheng(data['hadiths'][0]['text']);
+                }
+            })
+            .catch(err => console.log(err))
+
     }, [])
 
     return (
-        <p className="hadeeth-content">{hadeeth}</p>
+        <div id="hadeethContent">
+            <div id="hadeethContent__text">
+                <p id="ara">{hadeethara}</p>
+                <p id="eng"> {hadeetheng}</p>
+            </div>
+            <div id="hadeethContent__hadeethDetails"> {hadeethDetails} </div>
+        </div>
     )
 }
 
