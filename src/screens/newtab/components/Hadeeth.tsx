2d923fe4dd1e27ca31a615941a3f74e4b856c56b
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-11-14 17:07:03 +0200

    Add info icon

--- src/screens/newtab/components/Hadeeth.tsx
+++ src/screens/newtab/components/Hadeeth.tsx
@@ -16,7 +16,6 @@ function Hadeeth() {
             .then(data => {
                 if (data)
                 {
-                    console.log(data)
                     setHadeethara(data['hadiths'][0]['text']);
                     setHadeethDetails("Sahih al Bukhari, " + "Book #" + Object.keys(data["metadata"]["section"])[0]
                         + " " + Object.values(data["metadata"]["section"])[0] + ", Hadeeth #" + hadeethNum);
@@ -45,7 +44,19 @@ function Hadeeth() {
                 <p id="ara">{hadeethara}</p>
                 <p id="eng"> {hadeetheng}</p>
             </div>
-            <div id="hadeethContent__hadeethDetails"> {hadeethDetails} </div>
+            <div id="hadeethContent__hadeethDetails">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="#fff"
+                    style={{ float: "left", marginRight: "5px" }}
+
+                >
+                    <path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm0 18.5c-4.135 0-7.5-3.365-7.5-7.5S7.865 5.5 12 5.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5zm0-13c-.552 0-1 .448-1 1s.448 1 1 1 1-.448 1-1-.448-1-1-1zm1 9h-2v-6h2v6z" />
+                </svg>
+                {hadeethDetails} </div>
         </div>
     )
 }
