b8381602622f8a4976d1f93e7b7da6612c6af462
Fadi Asaad <firon1222@gmail.com>
2023-11-14 13:35:19 +0200

    update the newtab screen to use the updated components and the new systems

--- src/screens/newtab/NewTab.tsx
+++ src/screens/newtab/NewTab.tsx
@@ -1,30 +1,19 @@
 import "@/main.css";
 import Clock from "./components/Clock";
 import Background from "./components/Background";
-import QuickLinks from "./components/QuickLinks";
-import { useNewTabStorageScope } from "./storage-scopes";
-import { useState } from "react";
-import classNames from "classnames";
-import TodoSection from "./components/TodoSection";
 import { Button } from "./components/Buttons";
-
-function BackgroundSettings() {
-	const scope = useNewTabStorageScope();
-
-	return (
-		<input
-			type="url"
-			className="text-black bg-white/20 shadow shadow-white/50 p-1
-			rounded w-1/2 outline-none focus:bg-white/30"
-			placeholder="Background Image URL (empty for default)"
-			value={scope.backgroundImage || ""}
-			onChange={e => (scope.backgroundImage = e.target.value)}
-		/>
-	);
-}
+import QuickLinks from "./components/QuickLinks";
+import TodoSection from "./components/TodoSection";
+import {
+	OpenCloseSystem,
+	useOpenCloseSystem
+} from "@/features/OpenCloseSystem";
+import { SettingsDialog } from "./components/Settings";
 
 function NewTab() {
-	const [isTodoOpened, setIsTodoOpened] = useState(false);
+	const todoDialogSystem = useOpenCloseSystem("todoDialog");
+	const settingsDialogSystem = useOpenCloseSystem("settings");
+	const sessionsDialogSystem = useOpenCloseSystem("sessions");
 
 	return (
 		<div className="w-screen h-screen">
@@ -35,23 +24,38 @@ function NewTab() {
 			>
 				<div></div>
 				<Clock />
-				<BackgroundSettings />
 				<QuickLinks />
-				<div
-					className={classNames("w-96 h-3/4 absolute right-0 bottom-12", {
-						hidden: !isTodoOpened
-					})}
+				<OpenCloseSystem
+					systemId="todoDialog"
+					className="w-96 h-3/4 absolute right-2 bottom-12"
 				>
 					<TodoSection />
-				</div>
+				</OpenCloseSystem>
+				<OpenCloseSystem
+					systemId="settings"
+					className="w-8/12 h-3/4 absolute left-2 bottom-12"
+				>
+					<SettingsDialog />
+				</OpenCloseSystem>
 				<div className="w-full h-12 px-2 flex justify-between items-center">
-					<Button label="Settings" style="label" onClick={() => { }} />
+					<Button
+						label="Settings"
+						style="label"
+						onClick={() => { }}
+						openCloseSystem={settingsDialogSystem}
+					/>
 					<div className="space-x-4">
-						<Button label="Sessions" style="label" onClick={() => { }} />
+						<Button
+							label="Sessions"
+							style="label"
+							onClick={() => { }}
+							openCloseSystem={sessionsDialogSystem}
+						/>
 						<Button
 							label="Todo"
 							style="label"
-							onClick={() => setIsTodoOpened(s => !s)}
+							onClick={() => { }}
+							openCloseSystem={todoDialogSystem}
 						/>
 					</div>
 				</div>
