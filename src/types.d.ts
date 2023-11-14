ff6f97884a7edab5d194bc816dd350fd8ed4fa69
Fadi Asaad <firon1222@gmail.com>
2023-11-14 13:33:20 +0200

    define required types for the new systems

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,18 +1,38 @@
 export interface IQuickLink {
-	id: string;
-	title: string;
-	url: string;
-	icon?: string;
+  id: string;
+  title: string;
+  url: string;
+  icon?: string;
 }
 
 export interface ITodoItem {
-	id: number;
-	label: string;
-	done: boolean;
+  id: string;
+  label: string;
+  done: boolean;
 }
 
-export interface INewTabStorageScope {
-	quickLinks: IQuickLink[];
-	backgroundImage: string;
-	todoItems: ITodoItem[];
+export interface ISessionTab {
+  title: string;
+  url: string;
+  icon: string;
+}
+
+export interface ISessions {
+  [sessionName: string]: ISessionTab[];
+}
+
+export interface IStorageConfig {
+  quickLinks: IQuickLink[];
+  backgroundImage: string;
+  todoItems: ITodoItem[];
+  sessions: ISessions;
+  activeSession: string;
+}
+
+export interface IOpenCloseConfig {
+  todo: boolean;
+  settings: boolean;
+  sessions: boolean;
+  todoDialog: boolean;
+  quickLinksDialog: boolean;
 }
