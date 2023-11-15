783d5599be45bccc93d5ac11b4112ca9daf4758c
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-11-15 13:45:17 +0200

    Add comments and documentation to the Components

--- src/screens/newtab/components/Background.tsx
+++ src/screens/newtab/components/Background.tsx
@@ -1,10 +1,13 @@
+// This file defines the Background React Component of the new tab page. It is a full screen image.
 import { useStorageScope } from "@/features/StorageManager";
 
 function Background() {
+	// Get the background image from a user-inputed URL or a default image
 	const [image, _] = useStorageScope("backgroundImage");
 	const bgUrl = image || "/background2.jpg";
 
 	return (
+		// The background image is a fixed position div that covers the entire screen
 		<div className="top-0 left-0 z-[-1] fixed w-screen h-screen bg-black">
 			<div
 				className="w-full h-full absolute bg-cover"
